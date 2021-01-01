73ffaa7b7b5d945d70843c97090e83c11c5c732d
chris <larsenc83@gmail.com>
2021-01-01 13:43:48 -0500

    Import clean up in Author domain class

--- src/main/java/com/clarsen/sandbox/udemy/spring/springwebapp/domain/Author.java
+++ src/main/java/com/clarsen/sandbox/udemy/spring/springwebapp/domain/Author.java
@@ -7,8 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import org.hibernate.annotations.ManyToAny;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
